c21d27048088edded686f79a37e92971de54213b
Adarsha Paudel <132650292+ap21act@users.noreply.github.com>
2024-08-04 21:03:12 +0000

    Variables types

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=14453
+let accountEmail = "sushantbasnet2027@gmail.com"
+var accountPassword= "12345"
+
+accountCity = "Ktm"
+
+// accountId =2 // not allowed due to const 
+
+accountEmail="papa@gmail.com"
+accountPassword="23445"
+accountCity ="Pokhara"
+let accountState ;
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
