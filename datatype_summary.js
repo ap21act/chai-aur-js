8c7f44f44cd8eae3fdcd9c65f58b5d5569f1929c
Adarsha Paudel <132650292+ap21act@users.noreply.github.com>
2024-08-04 23:43:26 +0000

    dataType Summary

---
+++ datatype_summary.js
@@ -0,0 +1,35 @@
+/* Primitive dataType
+
+7 types: String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+*/
+
+/* Reference dataType (Non-primitive)
+
+types: Array, Objects, Functions
+
+*/
+
+const id =Symbol('123')
+const anotherId = Symbol("123")
+
+// False due to SYmbol functions as unique
+console.log(id ===anotherId)
+
+const bigNumber = 1234567890102839402223133442n
+
+const heros =["ironman","batman"] //array
+
+const obj ={
+    name:"hitesh",
+    age:22,
+    class:"tweleve",
+}
+ 
+let myFunction = function(){
+    console.log("Hello World")
+}
+console.log(typeof id);
+
+
+
