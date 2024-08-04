d0e4b4b6783358c850325c5c0fe28290beacb33a
Adarsha Paudel <132650292+ap21act@users.noreply.github.com>
2024-08-04 21:02:19 +0000

    DataType in Javascript with Notes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+/* Use strict to apply all code with newer version of JS but it's done almost everywhere now
+*/
+"use strict"
+// alert ("Hello World")
+// we are using nodejs can't use this only possible in browser
+
+let name="adarsha" // string dataType
+let age= 18 //Number dataType
+let isLoggedIn =true; // Boolean dataType
+
+
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "hitesh");
+
+
