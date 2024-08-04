fe43fe393a20cfa14bc6ff41c908f58c6883d066
Adarsha Paudel <132650292+ap21act@users.noreply.github.com>
2024-08-04 21:48:43 +0000

    operation and Difference between a++ and ++a

---
+++ 01_basics/03.01_operations.js
@@ -0,0 +1,30 @@
+let value =3
+let negValue = -value
+
+// console.log(value);
+
+// console.log(negValue);
+
+let str1 ="adar"
+let str2 ="sha"
+
+let str3 =str1+str2; //Execution of Concataination 
+
+// console.log('3'+2) //Still concatination 
+
+// console.log(3+2+'2') //3+2 happens and 2 is concatination becomes 52
+
+let gameCounter =300
+
+gameCounter++
+
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
--- 01_basics/03_conversionDataTypes.js
+++ 01_basics/03_conversionDataTypes.js
@@ -1,13 +1,13 @@
-let num = "false"
+// let num = "false"
 
-let valueOfNum = Number(num) //conversion from strring to Number 
+// let valueOfNum = Number(num) //conversion from strring to Number 
 
 
-console.log( typeof num);
+// console.log( typeof num);
 
-console.log( typeof valueOfNum); //checking dataTypes
+// console.log( typeof valueOfNum); //checking dataTypes
 
-console.log(valueOfNum);
+// console.log(valueOfNum);
 
 
 // "33" => 33
@@ -15,13 +15,20 @@ console.log(valueOfNum);
 // true => 1; false=> 0;
 // null => 0;
 
-let isLoggedIn = "";
+// let isLoggedIn = "";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=> true; 0 => false -23=> true 23 => true
 
 // ""=> false ; "ada"=> true
 
+let someNumber =undefined;
+
+let stringNumber = String(someNumber)
+
+console.log( stringNumber)
+
+// change in String is all into string for everything 
