e343bc2392b139e7297eeb5cb714c1923497b9c4
Adarsha Paudel <132650292+ap21act@users.noreply.github.com>
2024-08-05 01:55:58 +0000

    Date and Time

---
+++ 01_basics/07_dates_in_JS.js
@@ -0,0 +1,32 @@
+//Dates
+
+let myDate = new Date()
+
+// console.log(myDate.toDateString()) // output Mon Aug 05 2024
+// console.log(myDate.getFullYear()) //2024
+// console.log(myDate.toISOString()) // output 2024-08-05T01:02:14.507Z
+
+// console.log(myDate.toLocaleDateString()) //.  8/5/2024
+
+// console.log(myDate.toLocaleTimeString()) //.  1:03:26 AM
+
+// console.log(myDate.toLocaleString()) //.  8/5/2024, 1:04:24 AM
+
+// let myCreatedDate = new Date(2023, 0,23, 15,3) // output Mon Jan 23 2023 Month starts from 0 not 1 
+
+// let myCreatedDate = new Date ("2023-Jan-15")
+
+// console.log(myCreatedDate.toLocaleString())
+
+// let myTimeStamp = Date.now()
+
+// console.log(myCreatedDate.getTime());
+
+let newDate = new Date()
+
+console.log(newDate.getFullYear())
+
+
+
+
+
