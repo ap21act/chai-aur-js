a7e89da2e6c66689d370885e8ebeaf76cf7c960d
Adarsha Paudel <132650292+ap21act@users.noreply.github.com>
2024-08-05 00:53:13 +0000

    Math and Num with random as well between min and max

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,31 @@
+// const score = 500 //regular define of Number
+
+// const balance = new Number (100) //specifically Num define like typescript it is number 
+
+// console.log(score);
+
+// console.log(balance);
+
+
+// console.log(balance.toFixed(2)); //two decimal place i.e. 100.00
+
+// const otherNum= 1123.8664
+
+// const hundreds= 10000000000
+
+// 
+// console.log(otherNum.toPrecision(3)) // gives you three value from left rest round up or exponential i.e. 1123.8664 => 1.12e+3
+
+// console.log(hundreds.toLocaleString('en-IN')) // gives in indian style 
+
+
+//***************** ++++++++MATHs++++++++++++***** */
+
+// console.log(Math.abs(-4))
+console.log((Math.random()*10) +1)
+
+
+const min =10
+const max =20
+
+console.log(Math.floor((Math.random()*(max-min+1))+min)) // Use case random between numbers w.r.t Min and Max
